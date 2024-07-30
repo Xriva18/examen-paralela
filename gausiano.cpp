c37ff9b20b683bb573eae245e5009d2eb9f1eadb
Fernando <kevinguanoluisa2004@gmail.com>
2024-07-30 15:48:23 -0500

    algunos cambios

---
+++ gausiano.cpp
@@ -0,0 +1,15 @@
+#include "./lib-img/lib/lib-include.h"
+int main()
+{
+    Utility u;
+    StbImageImplementation stb;
+    GaussianFilter g;
+    int width, height;
+    double **image = stb.imread("/home/xriva20/Imágenes/prueba2.jpg", width, height); // Lectura de imagen
+    double sigma = 1;                                                                 // Valor ajustable
+    image = g.gaussianFilter(image, width, height, sigma);                            // Aplica filtro gaussiano
+    // Guardar imagen
+    stb.imwrite("imageGas.jpg", image, width, height);
+    // Liberar puntero
+    u.free_memory(image, height);
+}
