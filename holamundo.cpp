78b36ecb213531567604204c60fb6186099ab2bd
Fernando <kevinguanoluisa2004@gmail.com>
2024-07-29 20:16:17 -0500

    elimine hola mundo

--- holamundo.cpp
+++
@@ -1,9 +0,0 @@
-// esta es una prueba de manejo de git
-// esta es una miodifcaion 2
-// esta es una modificacion 3
-// esta es una modificacion 4
-// esta es una modificacion 5
-// esta es una modificacion 6
-// esta es una modificacion 7
-// esta es una modificacion 8
-// esta es una modificacion 9
