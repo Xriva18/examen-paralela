5c2d9a48555ff986e0f47c7a4fa60c58923915d3
Fernando <kevinguanoluisa2004@gmail.com>
2024-07-30 14:17:46 -0500

    cambios

---
+++ histograma.cpp
@@ -0,0 +1,14 @@
+#include "./lib-img/lib/lib-include.h"
+int main()
+{
+    Utility u;
+    StbImageImplementation stb;
+    HistogramHandler h; // instancia a la clase histograma
+    int width, height;
+    double **image = stb.imread("/home/xriva20/Imágenes/prueba2.jpg", width, height); // Lectura de imagen
+    int **histeq = h.histeq(image, width, height);                                    // equaliza el histograma de la imagen
+    // Guardar imagen
+    stb.imwrite("image.jpg", histeq, width, height);
+    // Liberar puntero
+    u.free_memory(histeq, height);
+}
